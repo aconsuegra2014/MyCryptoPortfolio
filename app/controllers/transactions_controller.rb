acc950373b726ef74ab0b5cb66cad8eac5e00e03
Arley Consuegra Roselló <arley.consuegra@electronicid.eu>
2021-10-17 14:57:21 -0400

    You can now write a note in the transaction

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -68,6 +68,6 @@ class TransactionsController < ApplicationController
     end
     # Only allow a list of trusted parameters through.
     def transaction_params
-      params.require(:transaction).permit(:crypto_quantity, :fiat_quantity, :date, :crypto_currency_id)
+      params.require(:transaction).permit(:crypto_quantity, :fiat_quantity, :date, :crypto_currency_id, :note)
     end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,6 @@
 class Transaction < ApplicationRecord
   belongs_to :crypto_currency
+  has_rich_text :note
+
   validates :crypto_quantity, presence: true
 end
